66c276379c7d9a5f8535d3a19b4c4b15e1d8cf1c
Ashwani <ashwani01082022@gmail.com>
2026-06-20 00:38:10 +0530

    update 20

--- src/components/home/BlogSection.jsx
+++ src/components/home/BlogSection.jsx
@@ -12,7 +12,7 @@ const BlogSection = ({ posts = [] }) => {
   if (published.length === 0) return null;
 
   return (
-    <section className="section-padding bg-black relative overflow-hidden">
+    <section className="section-padding bg-canvas/20 relative overflow-hidden">
       {/* Decorative */}
       <div className="absolute top-0 left-0 w-80 h-80 bg-ember/10 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2" />
       <div className="absolute bottom-0 right-0 w-96 h-96 bg-ember/5 rounded-full blur-3xl translate-x-1/3 translate-y-1/3" />
